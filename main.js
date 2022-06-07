a8bdd082046f73d3d26c5dfdc5482cfb026aae9d
sssssqew <sunrise.in.my.dream@gmail.com>
2022-06-07 22:58:42 +0900

    lecture 2 of self-driving car videos

---
+++ main.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById('myCanvas')
+canvas.width = 200
+
+const ctx = canvas.getContext('2d')
+const road = new Road(canvas.width/2, canvas.width*0.9)
+const car = new Car(road.getLaneCenter(3), 100, 30, 50)
+
+animate()
+
+function animate(){
+  canvas.height = window.innerHeight
+  car.update()
+
+  ctx.save()
+  ctx.translate(0, -car.y + canvas.height*0.7) // 자동차가 캔버스의 중앙점보다 조금 아래쪽에 항상 위치하도록 함
+
+  road.draw(ctx)
+  car.draw(ctx)
+
+  ctx.restore()
+  requestAnimationFrame(animate)
+}
